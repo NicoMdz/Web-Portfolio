010b3100c6a4c85f53cab66c033e3a7e9fc0a247
Nicolás Mendez Brito <nick.mdz.b@gmail.com>
2024-04-28 13:50:51 -0600

    Segundo Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,13 @@ export default function Header() {
     animation: "nav-shadown 1s linear both",
     animationTimeline: "scroll()",
     animationRange: "0 100px",
-  }} class="flex px-3 font-medium rounded-full text-white-600 dark:text-gray-200 justify-center items-center gap-x-4 shadow-lg ring-1 backdrop-blur dark:bg-gray-800/90 bg-white/50 ring-white/10">
+  }} class="flex px-3 font-medium rounded-full text-white  justify-center items-center gap-x-4 shadow-lg ring-1 backdrop-blur  bg-white/50 ring-white/10">
     <a href="#inicio">Inicio</a>
     <a href="#sobre-mi">Sobre mí</a>
     <a href="#experiencia">Experiencia</a>
     <a href="#proyectos">Proyectos</a>
     <a href="#tech-stack">Tech Stack</a>
-    <button>EN</button>
+    {/* <button>EN</button> */}
   </nav>
 </header>
   )
